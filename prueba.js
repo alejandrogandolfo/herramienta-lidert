e0341b951a0717008776bc7e9884a94e04bf6668
alejandrogandolfo <62605800+alejandrogandolfo@users.noreply.github.com>
2021-03-29 12:14:29 -0300

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,112 @@
+const open = document.getElementById('bremen');
+const open2 = document.getElementById('euro');
+const open3 = document.getElementById('palladino');
+const open4 = document.getElementById('lusqtoff');
+const open5 = document.getElementById('gamma');
+const open6 = document.getElementById('argen');
+const open7 = document.getElementById('rullman');
+const open8 = document.getElementById('david');
+const modal_container = document.getElementById('modal_container');
+const modal_container2 = document.getElementById('modal_container2');
+const modal_container3 = document.getElementById('modal_container3');
+const modal_container4 = document.getElementById('modal_container4');
+const modal_container5 = document.getElementById('modal_container5');
+const modal_container6 = document.getElementById('modal_container6');
+const modal_container7 = document.getElementById('modal_container7');
+const modal_container8 = document.getElementById('modal_container8');
+const cierro = document.getElementById('cerrar');
+const cierro2 = document.getElementById('cerrar2');
+const cierro3 = document.getElementById('cerrar3');
+const cierro4 = document.getElementById('cerrar4');
+const cierro5 = document.getElementById('cerrar5');
+const cierro6 = document.getElementById('cerrar6');
+const cierro7 = document.getElementById('cerrar7');
+const cierro8 = document.getElementById('cerrar8');
+
+open.addEventListener('click',()=>{
+	
+	modal_container.classList.add('show');
+	
+});
+
+cierro.addEventListener('click',()=>{
+	modal_container.classList.remove('show');
+	
+});
+
+
+
+open2.addEventListener('click',()=>{
+	
+	modal_container2.classList.add('show');
+	
+});
+cierro2.addEventListener('click',()=>{
+	modal_container2.classList.remove('show');
+	
+});
+
+open3.addEventListener('click',()=>{
+	
+	modal_container3.classList.add('show');
+	
+});
+cierro3.addEventListener('click',()=>{
+	modal_container3.classList.remove('show');
+
+});
+
+
+open4.addEventListener('click',()=>{
+	
+	modal_container4.classList.add('show');
+	
+});
+cierro4.addEventListener('click',()=>{
+	modal_container4.classList.remove('show');
+	
+});
+
+
+open5.addEventListener('click',()=>{
+	
+	modal_container5.classList.add('show');
+	
+});
+cierro5.addEventListener('click',()=>{
+	modal_container5.classList.remove('show');
+	
+});
+
+
+open6.addEventListener('click',()=>{
+	
+	modal_container6.classList.add('show');
+	
+});
+cierro6.addEventListener('click',()=>{
+	modal_container6.classList.remove('show');
+	
+});
+
+
+open7.addEventListener('click',()=>{
+	
+	modal_container7.classList.add('show');
+	
+});
+cierro7.addEventListener('click',()=>{
+	modal_container7.classList.remove('show');
+	
+});
+
+
+open8.addEventListener('click',()=>{
+	
+	modal_container8.classList.add('show');
+	
+});
+cierro8.addEventListener('click',()=>{
+	modal_container8.classList.remove('show');
+	
+});
